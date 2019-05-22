98e474549e33fc4ead20e22004b51d68c2a5c714
KalilDev <kalilgamer0@gmail.com>
2019-05-21 22:06:53 -0300

    Get rid of the shitty hack to get the old linearGradient lerp
    * Also animate if you change the colors at runtime on animatedGradient container

--- lib/views/animatedGradientContainer.dart
+++ lib/views/animatedGradientContainer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kalil_widgets/constants.dart';
-import 'oldGradient.dart' as old;
+import 'dart:ui';
 
 class AnimatedGradientContainer extends StatefulWidget {
   const AnimatedGradientContainer({
@@ -33,9 +33,13 @@ class AnimatedGradientContainer extends StatefulWidget {
 class _AnimatedGradientContainerState extends State<AnimatedGradientContainer>
     with TickerProviderStateMixin {
   AnimationController _controller;
-  Animation<old.LinearGradient> _animation;
+  Animation<LinearGradient> _animation;
 
   bool _isEnabled;
+
+  List<Color> _oldColors;
+  AnimationController _onChangeController;
+  Animation<double> _onChangeAnim;
 
   @override
   void initState() {
@@ -45,12 +49,19 @@ class _AnimatedGradientContainerState extends State<AnimatedGradientContainer>
     Animation curved =
         new CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
     _animation = new LinearGradientTween(
-        begin: old.LinearGradient(
+        begin: LinearGradient(
                 colors: widget.colors, stops: widget.falseValues),
             end:
-            old.LinearGradient(colors: widget.colors, stops: widget.trueValues))
+            LinearGradient(colors: widget.colors, stops: widget.trueValues))
         .animate(curved);
     _controller.value = widget.isEnabled ? 1.0 : 0.0;
+
+    _oldColors = widget.colors;
+    _onChangeController = new AnimationController(vsync: this, duration: widget.duration);
+    _onChangeAnim = new CurvedAnimation(parent: _onChangeController, curve: Curves.easeInOut);
+    _onChangeController.addListener(() {
+      if (_controller.status == AnimationStatus.completed || _controller.status == AnimationStatus.dismissed) _oldColors = widget.colors;
+    });
   }
 
   @override
@@ -64,40 +75,92 @@ class _AnimatedGradientContainerState extends State<AnimatedGradientContainer>
     Animation curved =
     new CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
     _animation = new LinearGradientTween(
-        begin: old.LinearGradient(
-            colors: widget.colors, stops: widget.falseValues),
+        begin: LinearGradient(
+            colors: _oldColors, stops: widget.falseValues),
         end:
-        old.LinearGradient(colors: widget.colors, stops: widget.trueValues))
+        LinearGradient(colors: _oldColors, stops: widget.trueValues))
         .animate(curved);
     if (_isEnabled != widget.isEnabled) {
       widget.isEnabled ? _controller.forward() : _controller.reverse();
       _isEnabled = widget.isEnabled;
     }
+    if (_oldColors != widget.colors) {
+      _onChangeController.value = 0.0;
+      _onChangeController.forward();
+    }
     return LayoutBuilder(
       builder: (context, constrains) => AnimatedBuilder(
         animation: _animation,
-        builder: (context, child) {
-          return Container(
-            height: widget.height,
-            width: widget.width,
-            decoration: old.BoxDecoration(
-              gradient: _animation.value,
-            ),
-            child: widget.child,
-          );
-        },
+        builder: (context, child) => AnimatedBuilder(
+            animation: _onChangeAnim,
+              builder: (context, child) => Container(
+              height: widget.height,
+              width: widget.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradientTween(begin: _animation.value, end: LinearGradient(colors: widget.colors, stops: _animation.value.stops)).lerp(_onChangeAnim.value),
+              ),
+              child: widget.child,
+          )
+        )
       ),
     );
   }
 }
 
-class LinearGradientTween extends Tween<old.LinearGradient> {
+class LinearGradientTween extends Tween<LinearGradient> {
   /// Provide a begin and end Gradient. To fade between.
   LinearGradientTween({
-    old.LinearGradient begin,
-    old.LinearGradient end,
+    LinearGradient begin,
+    LinearGradient end,
   }) : super(begin: begin, end: end);
 
   @override
-  old.LinearGradient lerp(double t) => old.LinearGradient.lerp(begin, end, t);
+  LinearGradient lerp(double t) {
+    assert(t != null);
+    if (begin == null && end == null) return null;
+    if (begin == null) return end.scale(t);
+    if (end == null) return begin.scale(1.0 - t);
+    final _ColorsAndStops interpolated =
+    _interpolateColorsAndStops(begin.colors, begin.stops, end.colors, end.stops, t);
+    return LinearGradient(
+      begin: AlignmentGeometry.lerp(begin.begin, end.begin, t),
+      end: AlignmentGeometry.lerp(begin.end, end.end, t),
+      colors: interpolated.colors,
+      stops: interpolated.stops,
+      tileMode: t < 0.5
+          ? begin.tileMode
+          : end.tileMode, // TODO(ianh): interpolate tile mode
+    );
+  }
+}
+
+class _ColorsAndStops {
+  _ColorsAndStops(this.colors, this.stops);
+
+  final List<Color> colors;
+  final List<double> stops;
+}
+
+_ColorsAndStops _interpolateColorsAndStops(List<Color> aColors,
+    List<double> aStops, List<Color> bColors, List<double> bStops, double t) {
+  assert(aColors.length == bColors.length,
+  'Cannot interpolate between two gradients with a different number of colors.');
+  assert((aStops == null && aColors.length == 2) ||
+      (aStops != null && aStops.length == aColors.length));
+  assert((bStops == null && bColors.length == 2) ||
+      (bStops != null && bStops.length == bColors.length));
+  final List<Color> interpolatedColors = <Color>[];
+  for (int i = 0; i < aColors.length; i += 1)
+    interpolatedColors.add(Color.lerp(aColors[i], bColors[i], t));
+  List<double> interpolatedStops;
+  if (aStops != null || bStops != null) {
+    aStops ??= const <double>[0.0, 1.0];
+    bStops ??= const <double>[0.0, 1.0];
+    assert(aStops.length == bStops.length);
+    interpolatedStops = <double>[];
+    for (int i = 0; i < aStops.length; i += 1)
+      interpolatedStops
+          .add(lerpDouble(aStops[i], bStops[i], t).clamp(0.0, 1.0));
+  }
+  return _ColorsAndStops(interpolatedColors, interpolatedStops);
 }
