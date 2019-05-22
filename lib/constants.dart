a57b3199a8439e8e9919ed2687ea64954b54c6c0
KalilDev <kalilgamer0@gmail.com>
2019-05-22 19:37:15 -0300

    Make the elevation smooth

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,6 +7,21 @@ class Constants {
   static const textTooltipTextSizeLess = 'Diminuir tamanho do texto';
   static const textTooltipTextSizePlus = 'Aumentar tamanho do texto';
 
+  // Material Guidelines
+  static const guidelinesDarkElevation = {
+    0: 0,
+    1: 5,
+    2: 7,
+    3: 8,
+    4: 9,
+    6: 11,
+    8: 12,
+    12: 14,
+    16: 15,
+    24: 16,
+    100: 16
+  };
+
   // Animation durations
   static const durationAnimationShort = Duration(milliseconds: 200);
   static const durationAnimationMedium = Duration(milliseconds: 400);
--- lib/views/elevatedContainer.dart
+++ lib/views/elevatedContainer.dart
@@ -1,6 +1,7 @@
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+import 'package:kalil_widgets/constants.dart';
 
 class ElevatedContainer extends StatelessWidget {
   final Widget child;
@@ -25,8 +26,8 @@ class ElevatedContainer extends StatelessWidget {
       this.transform,
       this.child,
       this.backgroundColor,
-        BorderRadius borderRadius,
-        Color elevatedColor,
+      BorderRadius borderRadius,
+      Color elevatedColor,
       @required this.elevation})
       : assert(margin == null || margin.isNonNegative),
         assert(padding == null || padding.isNonNegative),
@@ -35,37 +36,46 @@ class ElevatedContainer extends StatelessWidget {
             ? constraints?.tighten(width: width, height: height) ??
                 BoxConstraints.tightFor(width: width, height: height)
             : constraints,
-        this.elevatedColor = elevatedColor != null ? elevatedColor : Color(
-            0x50000000),
-        this.borderRadius = borderRadius != null ? borderRadius : BorderRadius
-            .circular(20.0),
+        this.elevatedColor =
+            elevatedColor != null ? elevatedColor : Color(0x50000000),
+        this.borderRadius =
+            borderRadius != null ? borderRadius : BorderRadius.circular(20.0),
         super(key: key);
+
+  double get alpha {
+    if (Constants.guidelinesDarkElevation.containsKey(elevation)) {
+      return Constants.guidelinesDarkElevation[elevation.floor()].toDouble();
+    } else {
+      List<double> keyList = [];
+      Constants.guidelinesDarkElevation.keys
+          .forEach((int) => keyList.add(int.toDouble()));
+      keyList.add(elevation);
+      keyList.sort();
+      final before = keyList[keyList.indexOf(elevation) - 1];
+      final after = keyList[keyList.indexOf(elevation) + 1];
+      final ratio = (elevation - before) / (after - before);
+
+      double k = -4;
+      final curvedRatio = (1 - math.pow(math.e, k * ratio));
+      // Interpolate between the previous and next value with the curve i want
+      return Constants
+          .guidelinesDarkElevation[keyList[keyList.indexOf(elevation) - 1]] +
+          (Constants.guidelinesDarkElevation[
+          keyList[keyList.indexOf(elevation) + 1]] -
+              Constants.guidelinesDarkElevation[
+              keyList[keyList.indexOf(elevation) - 1]]) *
+              curvedRatio;
+    }
+  }
 
   BoxDecoration materialCompliantElevation({Color bg, Brightness brightness}) {
     if (brightness == Brightness.dark) {
-      // Curve that approximates the material guidelines for dark mode
-      double k;
-      double adjust = 0;
-      if (3.5 > elevation) {
-        k = -6;
-        if (elevation < 2.5 && elevation >= 1.0) adjust = 1;
-      } else if (3.5 < elevation && elevation < 7.0) {
-        k = -5;
-      } else if (elevation > 7.0) {
-        k = -4;
-      }
-      final double toSixteen = 16 / (24 * (1 - math.pow(math.e, k * 1)));
-      final alpha = (24 * (1 - math.pow(math.e, k * elevation / 24)) *
-          toSixteen) + adjust;
-
       return BoxDecoration(
           borderRadius: BorderRadius.circular(20),
           color: Color.alphaBlend(
               Colors.white.withAlpha((alpha * 2.55).round()), bg));
     } else {
-      return BoxDecoration(
-          borderRadius: borderRadius,
-          color: bg);
+      return BoxDecoration(borderRadius: borderRadius, color: bg);
     }
   }
 
@@ -84,12 +94,8 @@ class ElevatedContainer extends StatelessWidget {
           decoration: materialCompliantElevation(
               bg: backgroundColor != null
                   ? backgroundColor
-                  : Theme
-                  .of(context)
-                  .backgroundColor,
-              brightness: Theme
-                  .of(context)
-                  .brightness),
+                  : Theme.of(context).backgroundColor,
+              brightness: Theme.of(context).brightness),
           padding: padding,
           child: child,
         ),
